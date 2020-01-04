bf389604be30897f1d96410aa389b3ced06bd2da
Julian Vega <juliandresvega0323@gmail.com>
2020-01-04 17:13:12 -0500

    Desarrollo modulo 03 - Spring MVC Data JPA

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1,'Angie', 'Cruz', 'acruz@ufps.edu.co', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2,'Julian', 'Becerra', 'julianandresbv@ufps.edu.co', '2020-01-04');
